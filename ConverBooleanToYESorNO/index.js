f19c3587e548139cb678f42eb209cbf710de5d6a
Kevin H <34373935+halekevi@users.noreply.github.com>
2022-11-19 17:01:45 -0500

    updates

---
+++ ConverBooleanToYESorNO/index.js
@@ -0,0 +1,10 @@
+function boolToWord( bool ){
+    //...
+    if ( bool == true){
+      return "Yes";
+    }else if (bool == false){
+      return "No";
+    }else {
+      return "IDK";
+    }
+  }
