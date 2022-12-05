c090448a1ced712d5effc2781a7b9c7f70869631
Kevin H <34373935+halekevi@users.noreply.github.com>
2022-12-05 08:53:31 -0500

    updates

---
+++ Find the middle element/index.js
@@ -0,0 +1,9 @@
+function gimme (triplet) {
+    if       (triplet[0] > triplet[1] && triplet[0] < triplet[2] || triplet[0] > triplet[2] && triplet[0] < triplet[1]) {
+      return 0
+    }else if (triplet[1] > triplet[0] && triplet[1] < triplet[2] || triplet[1] > triplet[2] && triplet[1] < triplet[0]) {
+      return 1
+    }else {
+      return 2
+    }
+    }
